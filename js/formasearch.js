43c304fe8a79ee0523ba3b304dba0130d6671fca
Asia Titova <Kutsanastasia@yandex.ru>
2020-03-02 19:44:46 +0700

    оы

--- js/formasearch.js
+++ js/formasearch.js
@@ -1,32 +1,37 @@
-<script src="../js/map.js"></s
-  
-  var button = document.querySelector(".offers-button");
-  var popup = document.querySelector(".form-show"); 
-  var checkin = popup.querySelector("[name=checkin]");
-  var checkout = popup.querySelector("[name=checkout]");
-  var adults = popup.querySelector("[name=adults]");
-  var children = popup.querySelector("[name=children]");
-  var form = popup.querySelector(".form-wrap");
 
-  button.addEventListener("click", function (evt) {
-      
+    
+var button = document.querySelector(".offers-button");
+var popup = document.querySelector(".form-show"); 
+var checkin = popup.querySelector("[name=checkin]");
+var checkout = popup.querySelector("[name=checkout]");
+var adults = popup.querySelector("[name=adults]");
+var children = popup.querySelector("[name=children]");
+var form = popup.querySelector(".form-wrap");
+
+if (popup) {
+  popup.classList.add("form-close");
+}
+
+button.addEventListener("click", function (evt) {
+    
+    evt.preventDefault();
+    popup.classList.toggle("form-show");
+
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!checkin.value || !checkout.value || !adults.value) {
+    evt.preventDefault();
+    console.log("Заполните форму регистрации!");
+  }
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("form-show")) {
       evt.preventDefault();
-      popup.classList.toggle("form-show");
-  });
-
-  form.addEventListener("submit", function (evt) {
-    if (!checkin.value || !checkout.value || !adults.value) {
-      evt.preventDefault();
-      console.log("Заполните форму регистрации!");
-    }
-  });
-
-
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (popup.classList.contains("form-show")) {
-        evt.preventDefault();
-        popup.classList.remove("form-show");
-      }
+      popup.classList.remove("form-show");
     }
   }
+});
